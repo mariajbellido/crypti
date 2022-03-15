e32b3de603f22100aa344dd236831b764612c743
Maria Jesus Bellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-15 13:41:37 +0100

    Creating a Card to map through the cryptocurrencies

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./Card.css";
-import axios from "axios";
-import { FiArrowUpRight, FiArrowDown } from "react-icons/fi";
+import { FiArrowDown, FiArrowUpRight } from "react-icons/fi";
 
-const Card = () => {
-  const [data, setData] = useState(null);
-
-  const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=6&page=1&sparkline=false";
-
-  useEffect(() => {
-    axios
-      .get(url)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  //console.log(data);
-
-  if (!data) return null;
-
+const Card = (props) => {
   return (
     <div className="card">
       <div className="top">
-        <img src={data[0].image} alt="cryptocurrency" />
+        <img src={props.data.image} alt="cryptocurrency" />
       </div>
       <div>
-        <h5>{data[0].name}</h5>
-        <p>{data[0].current_price.toLocaleString()}€</p>
+        <h5>{props.data.name}</h5>
+        <p>{props.data.current_price.toLocaleString()}€</p>
       </div>
-      {data[0].price_change_percentage_24h < 0 ? (
+      {props.data.price_change_percentage_24h < 0 ? (
         <span className="red">
           <FiArrowDown />
-          {data[0].price_change_percentage_24h.toFixed(2)}%
+          {props.data.price_change_percentage_24h.toFixed(2)}%
         </span>
       ) : (
         <span className="green">
           <FiArrowUpRight />
-          {data[0].price_change_percentage_24h.toFixed(2)}%
+          {props.data.price_change_percentage_24h.toFixed(2)}%
         </span>
       )}
     </div>
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Featured.css";
 import Card from "./Card";
+import axios from "axios";
 
 const Featured = () => {
+  const [data, setData] = useState(null);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=6&page=1&sparkline=false";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  //console.log(data);
+
+  if (!data) return null;
   return (
     <main className="featured">
       <section className="container">
@@ -14,7 +34,9 @@ const Featured = () => {
         </div>
         {/* Right side */}
         <div className="right">
-          <Card />
+          {data.map((data) => (
+            <Card key={data.id} />
+          ))}
         </div>
       </section>
     </main>
