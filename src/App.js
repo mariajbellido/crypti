8112e127c3847780d67435e0b63c377692e10611
Maria Jesus Bellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-15 17:24:23 +0100

    Working on footer Working on footer (JS - Css w/media queries)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Featured from "./components/Featured";
 import Signup from "./components/Signup";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Hero />
       <Featured />
       <Signup />
+      <Footer />
     </>
   );
 }
